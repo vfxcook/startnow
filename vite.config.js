33bf56f6ee666a4712f8e6d0abc90fab9141730e
vfxcook <itsvfxcook@gmail.com>
2025-04-28 22:07:49 +0530

    Update source files for latest website

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: command === 'serve' ? '/' : '/Website/', 
-}))
+  base: '/startnow/',  // <-- Important! Now change to /startnow/
+})
